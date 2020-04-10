ee28835aea476912ede704f257b39333d68daf13
Rajprabhu Arulnathan <rajprabhu@kcubeconsulting.com>
2020-04-10 16:47:59 +0530

    updated code for covid19

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ import Overlay from 'ol/Overlay';
 import VectorSource from 'ol/source/Vector';
 import {defaults as defaultControls, ZoomToExtent} from 'ol/control';
 import {Fill, Stroke, Style, Text} from 'ol/style';
-import {scaleSequential as d3scaleSequential,interpolateBlues as d3interpolateBlues,
-  scaleBand as d3ScaleBand, scaleLinear as d3scaleLinear,
-  axisBottom as d3axisBottom, axisLeft as d3axisLeft, event as d3event,
-   min as d3min, max as d3max, select as d3select} from 'd3';
 const https = require('https');
-var d3 = require("d3");
-var d3colorSequence = d3scaleSequential()
-            .interpolator(d3interpolateBlues);
+// var d3 = require("d3");
+var colorSequence = d3.scaleSequential()
+            .interpolator(d3.interpolateBlues);
 
 // 7582123.989659,  10842024.260498, 731252.369785, 4255931.279733
 var covid_details = null;
@@ -28,8 +24,16 @@ var createTextStyle = (feature) => {
     color: "#000"
   });
 };
-var filteredData = (name)=> covid_details.records.filter(d => d.name_of_state_ut == name);
+// variable for mover interaction ferature
+var selected = null;
 
+var tooltipDiv = d3.select('body').append('div')
+.attr('class', 'tooltip')
+.style('display', 'none');
+
+// filte function for available data
+var filteredData = (name)=> covid_details.records.filter(d => d.name_of_state_ut == name);
+// return color based on value
 var createColor = (feature) => {
   if (covid_details == null){
     return  'rgba(255, 255, 255, 0.6)'
@@ -37,15 +41,14 @@ var createColor = (feature) => {
     let data = filteredData( feature.get('ST_NM'));
     // covid_details.records.filter(d => d.name_of_state_ut == feature.get('ST_NM'));
     if (data.length){
-      return d3colorSequence(parseInt(data[0].total_confirmed_cases));
-    //   return new Fill({
-    //     color: d3colorSequence(parseInt(data[0].total_confirmed_cases))
-    //   });
+      return colorSequence(parseInt(data[0].total_confirmed_cases));
+
     }
     return  'rgba(255, 255, 255, 0.6)';
   }
 };
 
+// Default style
 var indiaStyle = (feature, resolution)=>{
   return new Style({
     fill: new Fill({
@@ -70,95 +73,7 @@ var indiaStyle = (feature, resolution)=>{
   });
 };
 
-
-
-var vector = new VectorLayer({
-  source: new VectorSource({
-    url: './data/top.json',
-    format: new TopoJSON({
-      featureProjection: 'EPSG:3857'
-    }),
-    overlaps: false
-  }),
-  style: indiaStyle
-  //  function(feature) {
-  //   style.getText().setText(feature.get('ST_NM'));
-  //   return style;
-  // }
-});
-
-var map = new Map({
-  controls: defaultControls().extend([
-    new ZoomToExtent({
-      extent: [
-        7083572.2852,670199.8640, 10842024.260498, 4255931.279733
-      ]
-    })
-  ]),
-  layers: [vector],
-  target: 'map',
-  view: new View({
-    center: [8649002.624524, 2377497.327782],
-  }),
-});
-map.getView().fit([7083572.2852,670199.8640, 10842024.260498, 4255931.279733], { constrainResolution: true });
-
-https.get('https://api.data.gov.in/resource/cd08e47b-bd70-4efb-8ebc-589344934531?format=json&limit=all&api-key=579b464db66ec23bdd000001cdc3b564546246a772a26393094f5645',(res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-  var body = '';
-  res.on('data', (chunk) =>{
-    body += chunk;
-  });
-  res.on('end', function() {
-    console.log(body);
-    covid_details = JSON.parse(body);
-    let initial_val = 0;
-    total_cases = covid_details.records.reduce((total, curr) =>{
-      total += parseInt(curr.total_confirmed_cases);
-      return total;
-    }, initial_val);
-    d3select('#confirmCases').html(total_cases);
-    cured = covid_details.records.reduce((total, curr) =>{
-      total += parseInt(curr.cured_discharged_migrated);
-      return total;
-    }, initial_val);
-    d3select('#curedCases').html(cured);
-    death = covid_details.records.reduce((total, curr) =>{
-      total += parseInt(curr.death);
-      return total;
-    }, initial_val);
-    d3select('#deathCases').html(death);
-    let min = d3min(covid_details.records, d => parseInt(d.total_confirmed_cases));
-    let max = d3max(covid_details.records, d => parseInt(d.total_confirmed_cases));
-    d3colorSequence.domain([min, max+200]);
-    console.log(covid_details);
-    vector.style = updateMapColor();
-    var sorted_data = covid_details.records.sort((a, b) => parseInt(a.s_no) < parseInt(b.s_no) ? 1: -1);
-    covid_details.records = sorted_data.reverse();
-    var top5Cases = covid_details.records.sort((a, b) => parseInt(a.total_confirmed_cases) < parseInt(b.total_confirmed_cases) ? 1: -1)
-                        .slice(0,5);
-    console.log(top5Cases);
-    highestcaseChart(top5Cases);
-    pieChart(covid_details.records);
-    createDatatable(covid_details);
-  });
-}).on('error', (e) =>{
-  console.error(e);
-});
-
-function updateMapColor(feature){
-  // vector.getSource().forEachFeature((feature)=>{
-  //   console.log(feature);
-  // });
-  vector.getSource().refresh();
-  d3.select('.pageloader').classed("is-active", false);
-  // style.getText().setText(feature.get('ST_NM'));
-  // return style;
-}
-
-var selected = null;
-
+// hight light for chart on hover
 var indiaHighlightStyle = (feature)=>{
   return new Style({
     fill: new Fill({
@@ -172,61 +87,21 @@ var indiaHighlightStyle = (feature)=>{
     text: createTextStyle(feature)
   });
 };
-map.on(['pointermove', 'singleclick'], function(e) {
-  if (selected !== null) {
-    selected.setStyle(undefined);
-    selected = null;
 
-  }
+// refresh topojson
+function updateMapColor(feature){
+  // vector.getSource().forEachFeature((feature)=>{
+  //   console.log(feature);
+  // });
+  vector.getSource().refresh();
+  d3.select('.pageloader').classed("is-active", false);
+  // style.getText().setText(feature.get('ST_NM'));
+  // return style;
+}
 
-  map.forEachFeatureAtPixel(e.pixel, function(f) {
-    popup.getElement().innerHTML = '';
-    selected = f;
-    f.setStyle(indiaHighlightStyle);
-    let data = filteredData( selected.get('ST_NM'));
-    popup.getElement().innerHTML = constructTable(data[0]);
-    popup.setPosition(e.coordinate);
-   
-    popup.setPositioning('bottom-left');
-    if(window.screen.width <=520){
-      popup.setPositioning('center-center');
-    }
-    return selected;
-  });
-  // var element = popup.getElement();
-  const delta = getOverlayOffsets(map, popup);
-  // popup.setPosition(e.coordinate);
-  if(delta[0] < 0) {
-    popup.setPositioning('top-right');
-    if(window.screen.width <=520){
-      popup.setPositioning('top-right');
-    }
-  }
-  else if(delta[0] == 0 && delta[1]>0) {
-    popup.setPositioning('top-left');
-    if(window.screen.width <=520){
-      popup.setPositioning('center-left');
-    }
-  }
-  // else if(delta[1] ==0 && delta[0]==0){
-  //   popup.setPositioning('top-left');
-  // }
-  // popup.setOffset(delta);
-
-  popup.getElement().style.display = selected ? '' : 'none';
-  document.body.style.cursor = selected ? 'pointer' : '';
-});
-
-
-// Popup showing the position the user clicked
-var popup = new Overlay({
-  element: document.getElementById('popup'),
-  positioning: 'bottom-left'
-});
-map.addOverlay(popup);
+// table structure for state hover or click event
 var constructTable = function(data){
-
-  let header = "<table class='table is-narrow is-bordered is-responsive'> \
+  let header = "<table class='table is-narrow is-bordered''> \
   <thead> \
     <tr> \
     <th>Field</th> \
@@ -284,8 +159,82 @@ let mobile_body =
     <td>${data.date_time.split(' ')[0] + '<br/>' + data.date_time.split(' ')[1]}</td> \
     </tr> </tbody></table>`;
 return header+mobile_body;
+};
+
+// get request from data.gov,in
+https.get('https://api.data.gov.in/resource/cd08e47b-bd70-4efb-8ebc-589344934531?format=json&limit=all&api-key=579b464db66ec23bdd000001cdc3b564546246a772a26393094f5645',(res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+  var body = '';
+  res.on('data', (chunk) =>{
+    body += chunk;
+  });
+  res.on('end', function() {
+    console.log(body);
+    covid_details = JSON.parse(body);
+    let initial_val = 0;
+    // sum for main display
+    total_cases = covid_details.records.reduce((total, curr) =>{
+      total += parseInt(curr.total_confirmed_cases);
+      return total;
+    }, initial_val);
+    d3.select('#confirmCases').html(total_cases);
+    cured = covid_details.records.reduce((total, curr) =>{
+      total += parseInt(curr.cured_discharged_migrated);
+      return total;
+    }, initial_val);
+    d3.select('#curedCases').html(cured);
+    death = covid_details.records.reduce((total, curr) =>{
+      total += parseInt(curr.death);
+      return total;
+    }, initial_val);
+    d3.select('#deathCases').html(death);
+    // min max for color creation
+    let min = d3.min(covid_details.records, d => parseInt(d.total_confirmed_cases));
+    let max = d3.max(covid_details.records, d => parseInt(d.total_confirmed_cases));
+    colorSequence.domain([min, (max+200)]);
+    //  update style for map
+    vector.style = updateMapColor();
+
+    var sorted_data = covid_details.records.sort((a, b) => parseInt(a.s_no) < parseInt(b.s_no) ? 1: -1);
+    covid_details.records = sorted_data.reverse();
+    // top for bar chart most cases
+    var top5Cases = covid_details.records.sort((a, b) => parseInt(a.total_confirmed_cases) < parseInt(b.total_confirmed_cases) ? 1: -1)
+                        .slice(0,5);
+    // console.log(top5Cases);
+    highestcaseChart(top5Cases);
+    // pie chart data update
+    pieChart(covid_details.records);
+    createDatatable(covid_details);
+  });
+}).on('error', (e) =>{
+  console.error(e);
+});
+
+
+// chart events
+function mouseover(){
+  tooltipDiv.style('display', 'inline');
 }
 
+function mousemove(){
+  var d = d3.select(this).data()[0]
+  tooltipDiv
+      .html(d.name_of_state_ut + '<br/>' + d.total_confirmed_cases)
+      .style('left', (d3.event.pageX -10) + 'px')
+      .style('top', (d3.event.pageY - 12) + 'px');
+  
+  setTimeout(()=>{
+    mouseout();
+  },5000);
+}
+
+function mouseout(){
+tooltipDiv.style('display', 'none');
+}
+
+
+// changes of position for overlay popup
 var getOverlayOffsets = function (mapInstance, overlay) {
   const overlayRect = overlay.getElement().getBoundingClientRect();
   const mapRect = mapInstance.getTargetElement().getBoundingClientRect();
@@ -315,8 +264,10 @@ var getOverlayOffsets = function (mapInstance, overlay) {
   return(delta);
 };
 
+// create bar chart
 function highestcaseChart(data){
   let element = document.getElementById("mostCasesBarChart");
+  d3.select("#mostCasesBarChart svg").remove();
   let width = +element.clientWidth;
   element.style.height = (width/2) + 'px';
   // element.clientHeight = +element.clientHeight+50
@@ -350,7 +301,7 @@ function highestcaseChart(data){
 
   // Add Y axis
   var y = d3.scaleLinear()
-  .domain([0, d3max(data, (d)=> d.total_confirmed_cases)])
+  .domain([0, d3.max(data, (d)=> parseInt(d.total_confirmed_cases)+100)])
   .range([ height, 0]);
   svg.append("g")
   .attr("class", "axis")
@@ -364,14 +315,13 @@ function highestcaseChart(data){
   .attr("x", function(d) { return x(d.name_of_state_ut); })
   .attr("width", x.bandwidth())
   .attr("fill", (d)=> {
-          return d3colorSequence(d.total_confirmed_cases)})
+          return colorSequence(d.total_confirmed_cases)})
   // no bar at the beginning thus:
   .attr("height", function(d) { return height - y(0); }) // always equal to 0
   .attr("y", function(d) { return y(0); })
   .on('mouseover', mouseover)
-        .on('mousemove', mousemove)
-        .on('mouseout', mouseout)
-        .on('click', clickEve)
+  .on('click', mousemove);
+
   // Animation
   svg.selectAll("rect")
   .transition()
@@ -397,38 +347,11 @@ function highestcaseChart(data){
         .text("States");
  }
 
-var tooltipDiv = d3select('body').append('div')
-.attr('class', 'tooltip')
-.style('display', 'none');
-
-function mouseover(){
-    tooltipDiv.style('display', 'inline');
-}
-function mousemove(){
-    var d = d3select(this).data()[0]
-    tooltipDiv
-        .html(d.name_of_state_ut + '<br/>' + d.total_confirmed_cases)
-        .style('left', (d3event.pageX -10) + 'px')
-        .style('top', (d3event.pageY - 12) + 'px');
-}
-function mouseout(){
-  tooltipDiv.style('display', 'none');
-}
-function clickEve(){
-  mouseover();
-  var d = d3select(this).data()[0]
-  tooltipDiv
-      .html(d.name_of_state_ut + '<br/>' + d.total_confirmed_cases)
-      .style('left', (d3event.pageX -10) + 'px')
-      .style('top', (d3event.pageY - 12) + 'px');
-      setTimeout(()=>{
-        mouseout();
-      },3000);
-}
-
+// create pie charts
 function pieChart(rawData){
   // var margin = { left: 40, top: 0, right: 10, bottom: 50 };
   let element = document.getElementById("pieChart");
+  d3.select("#pieChart svg").remove();
   let width = +element.clientWidth -50 ;
   let height = +element.clientWidth -50;
   let r = 100;
@@ -457,6 +380,7 @@ function pieChart(rawData){
         .append("svg")
         .attr("viewBox", [-width / 2, -height / 2, width, height]);
 
+
   svg.append("g")
         .attr("stroke", "white")
       .selectAll("path")
@@ -464,25 +388,19 @@ function pieChart(rawData){
       .join("path")
         .attr("fill", d => color(d.data.name_of_state_ut))
         .attr("d", arc)
-      .append("title")
-        .text(d => `${d.data.name_of_state_ut}: ${d.data.death}`)
-        .on("mouseover", function (d) {
+        .on("click", function (d) {
           d3.select(".tooltip")
               .style("left", d3.event.pageX + "px")
               .style("top", d3.event.pageY + "px")
-              .style("opacity", 1)
               .style('font-size', '12px')
-              .select("#value")
-              .text(d.value);
+              .text(`${d.data.name_of_state_ut}: ${d.data.death}`);
+          setTimeout(()=>{
+                mouseout();
+              },3000);
       })
-          .on("mouseout", function () {
-          // Hide the tooltip
-          d3.select(".tooltip")
-              .style("opacity", 0);;
-      });  
+      .on('mouseover', mouseover);
+      // .on('mouseover', mouseover);
 
-
-  
         svg.append("g")
         .attr("font-family", "sans-serif")
         .attr("font-size", 12)
@@ -497,13 +415,15 @@ function pieChart(rawData){
             .text(d => d.data.death));
 }
 
+// create data table for Map
 function createDatatable(data){
   var th_rows = '';
   var td_rows = '';
+  d3.select("#tableViewTitle").html(data.title)
   data.field.forEach((field) =>{
     th_rows+=`<th>${field.name}</th>`;
   });
-  d3select('#tableView thead tr').html(th_rows);
+  d3.select('#tableView thead tr').html(th_rows);
   data.records.forEach((d) =>{
     td_rows += `<tr>
       <td>${d.s_no}</td>
@@ -515,5 +435,102 @@ function createDatatable(data){
       <td>${d.date_time}</td>
     </tr>`
   });
-  d3select('#tableView tbody').html(td_rows);
+  d3.select('#tableView tbody').html(td_rows);
 }
+
+// vector layer creation
+var vector = new VectorLayer({
+  source: new VectorSource({
+    url: './data/top.json',
+    format: new TopoJSON({
+      featureProjection: 'EPSG:3857'
+    }),
+    overlaps: false
+  }),
+  style: indiaStyle
+  //  function(feature) {
+  //   style.getText().setText(feature.get('ST_NM'));
+  //   return style;
+  // }
+});
+
+// map data
+var map = new Map({
+  controls: defaultControls().extend([
+    new ZoomToExtent({
+      extent: [
+        7083572.2852,670199.8640, 10842024.260498, 4255931.279733
+      ]
+    })
+  ]),
+  layers: [vector],
+  target: 'map',
+  view: new View({
+    center: [8649002.624524, 2377497.327782],
+  }),
+});
+map.getView().fit([7083572.2852,670199.8640, 10842024.260498, 4255931.279733], { constrainResolution: true });
+
+// pointer move click for state value display
+map.on(['pointermove', 'singleclick'], function(e) {
+  if (selected !== null) {
+    selected.setStyle(undefined);
+    selected = null;
+
+  }
+
+  map.forEachFeatureAtPixel(e.pixel, function(f) {
+    popup.getElement().innerHTML = '';
+    selected = f;
+    f.setStyle(indiaHighlightStyle);
+    let data = filteredData( selected.get('ST_NM'));
+    popup.getElement().innerHTML = constructTable(data[0]);
+    popup.setPosition(e.coordinate);
+   
+    popup.setPositioning('bottom-left');
+    if(window.screen.width <=520){
+      popup.setPositioning('center-center');
+    }
+    return selected;
+  });
+  // var element = popup.getElement();
+  const delta = getOverlayOffsets(map, popup);
+  // popup.setPosition(e.coordinate);
+  if(delta[0] < 0) {
+    popup.setPositioning('top-right');
+    if(window.screen.width <=520){
+      popup.setPositioning('top-right');
+    }
+  }
+  else if(delta[0] == 0 && delta[1]>0) {
+    popup.setPositioning('top-left');
+    if(window.screen.width <=520){
+      popup.setPositioning('center-left');
+    }
+  }
+  // else if(delta[1] ==0 && delta[0]==0){
+  //   popup.setPositioning('top-left');
+  // }
+  // popup.setOffset(delta);
+
+  popup.getElement().style.display = selected ? '' : 'none';
+  document.body.style.cursor = selected ? 'pointer' : '';
+});
+
+// Popup showing the position the user clicked
+var popup = new Overlay({
+  element: document.getElementById('popup'),
+  positioning: 'bottom-left'
+});
+map.addOverlay(popup);
+
+
+window.addEventListener("resize", function(event) {
+  map.getView().fit([7083572.2852,670199.8640, 10842024.260498, 4255931.279733], { constrainResolution: true });
+  var top5Cases = covid_details.records.sort((a, b) => parseInt(a.total_confirmed_cases) < parseInt(b.total_confirmed_cases) ? 1: -1)
+  .slice(0,5);
+  // console.log(top5Cases);
+  highestcaseChart(top5Cases);
+  // pie chart data update
+  pieChart(covid_details.records);
+})
